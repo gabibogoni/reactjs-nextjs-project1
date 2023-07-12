836451a8a945835591aad99935383fcde4ae793c
Gabrieli <gabi.bogoni2@gmail.com>
2023-07-12 19:05:54 -0300

    state com arrays

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-import logo from './logo.svg';
 import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   //criando um estado
   state = {
-    name: "Gabrieli Bogoni",
-    counter: 0
+    posts: [
+      {
+        id: 1,
+        title: "O título 1",
+        body: "O body 1"
+      },
+      {
+        id: 2,
+        title: "O título 2",
+        body: "O body 2"
+      },
+      {
+        id: 3,
+        title: "O título 3",
+        body: "O body 3"
+      }
+    ]
   };
-
-  handlePClick = () => {
-    //sempre quando o estado muda no React a função render vai ser chamada novamente (chamei o estado render vai ser chamado novamente com o estado novo)
-    this.setState({name: "Fellipe Carvalho"});
-  }
-
-  handleAClick = (event) => {
-    /*para não precisar fazer um bind do método utilizamos um hack do JS onde é criado um método como arrow function 
-    (a arrow function não tem this dentro dela, então significa que quando eu chamar o this, ela vai buscar dentro do elemento pai, que
-    nesse caso é a própria classe)*/
-    event.preventDefault();
-    const {counter} = this.state;
-    this.setState({counter: counter + 1});
-  }
 
   render() {
     /*sintaxe de destructuring*/
-    const {name, counter} = this.state;
+    const {posts} = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {/*para colocar uma var no jsx {var} */}
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link
-          </a>
-        </header>
+        {posts.map(apelido_do_item_do_array => (
+          <div key={apelido_do_item_do_array.id}>
+            <h1>{apelido_do_item_do_array.title}</h1>
+            <p>{apelido_do_item_do_array.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
