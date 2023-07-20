754a14e5db9da6970c8e1cd83b0ca4a69fb0716e
Gabrieli <gabi.bogoni2@gmail.com>
2023-07-20 18:19:02 -0300

    metodos de ciclo de vida

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 class App extends Component {
   //criando um estado
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -23,12 +24,40 @@ class App extends Component {
     ]
   };
 
+  timeoutUpdate = null;
+
+  /*quando o componente for montado na tela eu quero que aconteça alguma coisa, o componentDidMount() vai ser montado uma vez assim que o componente
+  for montado na tela*/
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    /*looping infinito (o componentDidMount() dispara 1x, o componente atualiza, como nesse caso ele atualizou, o método componentDidUpdate() vai ser
+    usado e vai chamar de novo essa função e vira um looping infinito)*/
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const {posts, counter} = this.state;
+    posts[0].title = "O título mudou";
+    
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({posts, counter: counter + 1});
+    }, 1000)
+  }
+ 
   render() {
     /*sintaxe de destructuring*/
-    const {posts} = this.state;
+    const {posts, counter} = this.state;
 
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map(apelido_do_item_do_array => (
           <div key={apelido_do_item_do_array.id}>
             <h1>{apelido_do_item_do_array.title}</h1>
