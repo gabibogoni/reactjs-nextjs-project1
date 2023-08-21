ac0c678698a9d20726d63ec744390c7a440e9c93
Gabrieli Bogoni <gabi.bogoni2@gmail.com>
2023-08-21 17:18:03 -0300

    Introdução a testes com o Jest e Testing Library - teste componente Button

---
+++ src/templates/Home/Home.spec.jsx
@@ -0,0 +1,7 @@
+ /*geralmente se coloca a descrição do componente (no caso de teste a componentes) a ser testado e dentro do describe são colocados os
+ testes deste componente*/
+describe('<Home />', () => {
+  it('is a dummy test', () => {
+    expect(1).toBe(1);
+  })
+})
