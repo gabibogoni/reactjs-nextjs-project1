a90e2d15ac42101b7e99a39c6eaabbd0e1e87e19
Gabrieli Bogoni <gabi.bogoni2@gmail.com>
2023-08-21 18:20:02 -0300

    Introdução a testes com o Jest e Testing Library - teste componente PostCard

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -5,7 +5,7 @@ export const PostCard = ({imagem, title, id, body}) => {
         <div className="post">
             <img src={imagem} alt={title} />
             <div className="post-content">
-                <h2>{title}</h2>
+                <h2>{title} {id}</h2>
                 <p>{body}</p>
             </div>
         </div>
