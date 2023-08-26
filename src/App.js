5fb1b2ee0aee443cdb8f766ad344ec03ef8dab44
Gabrieli Bogoni <gabi.bogoni2@gmail.com>
2023-08-26 09:45:09 -0300

    teste main

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React today
           </a>
         </header>
       </div>
